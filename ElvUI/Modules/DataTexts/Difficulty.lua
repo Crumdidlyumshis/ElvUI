50c215ffdce3bd12dfb31bb98a78fc5b4e91bd21
Crum <stschleyer89@gmail.com>
2024-07-14 15:34:08 -0500

    update difficulty datatext

--- ElvUI/Modules/DataTexts/Difficulty.lua
+++ ElvUI/Modules/DataTexts/Difficulty.lua
@@ -2,145 +2,82 @@ local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
 
 local _G = _G
-local pairs, format, tinsert = pairs, format, tinsert
+local format = format
 
+local GetDungeonDifficulty = GetDungeonDifficulty
+local GetRaidDifficulty = GetRaidDifficulty
+local SetDungeonDifficulty = SetDungeonDifficulty
+local SetRaidDifficulty = SetRaidDifficulty
+local GetInstanceInfo = GetInstanceInfo
+local ResetInstances = ResetInstances
 local EasyMenu = EasyMenu
-local GetDungeonDifficultyID, GetRaidDifficultyID, GetLegacyRaidDifficultyID = GetDungeonDifficultyID, GetRaidDifficultyID, GetLegacyRaidDifficultyID
-local SetDungeonDifficultyID, SetRaidDifficultyID, SetLegacyRaidDifficultyID = SetDungeonDifficultyID, SetRaidDifficultyID, SetLegacyRaidDifficultyID
-local GetInstanceInfo, GetDifficultyInfo, ResetInstances = GetInstanceInfo, GetDifficultyInfo, ResetInstances
-local C_ChallengeMode_GetActiveChallengeMapID = E.Retail and C_ChallengeMode.GetActiveChallengeMapID
-local C_ChallengeMode_GetActiveKeystoneInfo = E.Retail and C_ChallengeMode.GetActiveKeystoneInfo
-local C_ChallengeMode_IsChallengeModeActive = E.Retail and C_ChallengeMode.IsChallengeModeActive
-local C_MythicPlus_IsMythicPlusActive = E.Retail and C_MythicPlus.IsMythicPlusActive
 
-local DungeonTexture, RaidTexture, LegacyTexture = CreateAtlasMarkup('Dungeon', 20, 20), CreateAtlasMarkup('Raid', 20, 20), CreateAtlasMarkup('worldquest-icon-raid', 20, 20)
-local DungeonDifficultyID, RaidDifficultyID, LegacyRaidDifficultyID = GetDungeonDifficultyID(), GetRaidDifficultyID(), E.Retail and GetLegacyRaidDifficultyID()
+local heroicTex = [[|Tinterface\lfgframe\ui-lfg-icon-heroic:20:20:0:0:64:64:0:36:0:36|t]]
+local dungTex = [[|Tinterface\icons\spell_arcane_teleportstormwind:20:20:0:0:64:64:4:60:4:60|t]]
+local raidTex = [[|Tinterface\icons\spell_arcane_teleportshattrath:20:20:0:0:64:64:4:60:4:60|t]]
 
-local RightClickMenu, DiffLabel = {
-	{ text = _G.DUNGEON_DIFFICULTY, isTitle = true, notCheckable = true },
-	{ text = _G.PLAYER_DIFFICULTY1, checked = function() return GetDungeonDifficultyID() == 1 end, func = function() SetDungeonDifficultyID(1) end },
-	{ text = _G.PLAYER_DIFFICULTY2, checked = function() return GetDungeonDifficultyID() == 2 end, func = function() SetDungeonDifficultyID(2) end },
-	{ text = '', isTitle = true, notCheckable = true },
-	{ text = _G.RAID_DIFFICULTY, isTitle = true, notCheckable = true},
-	{ text = _G.PLAYER_DIFFICULTY1, checked = function() return GetRaidDifficultyID() == 14 end, func = function() SetRaidDifficultyID(14) end },
-	{ text = _G.PLAYER_DIFFICULTY2, checked = function() return GetRaidDifficultyID() == 15 end, func = function() SetRaidDifficultyID(15) end },
-	{ text = '', isTitle = true, notCheckable = true },
-	{ text = _G.RESET_INSTANCES, notCheckable = true, func = function() ResetInstances() end},
-}, {}
-
-if E.Retail then
-	tinsert(RightClickMenu, 4, { text = _G.PLAYER_DIFFICULTY6, checked = function() return GetDungeonDifficultyID() == 23 end, func = function() SetDungeonDifficultyID(23) end })
-	tinsert(RightClickMenu, 9, { text = _G.PLAYER_DIFFICULTY6, checked = function() return GetRaidDifficultyID() == 16 end, func = function() SetRaidDifficultyID(16) end })
-	tinsert(RightClickMenu, 10, { text = '', isTitle = true, notCheckable = true })
-	tinsert(RightClickMenu, 11, { text = _G.UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_LEGACY_RAID, isTitle = true, notCheckable = true })
-	tinsert(RightClickMenu, 12, { text = _G.RAID_DIFFICULTY1, checked = function() return GetLegacyRaidDifficultyID() == 3 end, func = function() SetLegacyRaidDifficultyID(3) end })
-	tinsert(RightClickMenu, 13, { text = _G.RAID_DIFFICULTY1..' '.._G.PLAYER_DIFFICULTY2, checked = function() return GetLegacyRaidDifficultyID() == 5 end, func = function() SetLegacyRaidDifficultyID(5) end })
-	tinsert(RightClickMenu, 14, { text = _G.RAID_DIFFICULTY2, checked = function() return GetLegacyRaidDifficultyID() == 4 end, func = function() SetLegacyRaidDifficultyID(4) end })
-	tinsert(RightClickMenu, 15, { text = _G.RAID_DIFFICULTY2..' '.._G.PLAYER_DIFFICULTY2, checked = function() return GetLegacyRaidDifficultyID() == 6 end, func = function() SetLegacyRaidDifficultyID(6) end })
-end
-
-for i = 1, 200 do
-	local Name = GetDifficultyInfo(i)
-	if Name and not DiffLabel[i] then
-		DiffLabel[i] = Name
-	end
-end
-
-local DiffIDLabel = {
-	['N'] = { 1, 14, 38 },
-	['H'] = { 2, 15, 39 },
-	['M'] = { 16, 23, 40 },
-	['10N'] = { 3 },
-	['25N'] = { 4 },
-	['10H'] = { 5 },
-	['25H'] = { 6 },
-	['LFR'] = { 7, 17 },
-	['CM'] = { 8 },
-	['40'] = { 9 },
-	['TW'] = { 24, 33, 151 },
-	['S'] = { 11, 12, 20, 152, 153 },
-	['E'] = { 18, 19, 30 },
-	['PvP'] = { 25, 29, 32, 34, 45 },
-	['WF'] = { 147 },
-	['WFH'] = { 149 },
+local RightClickMenu = {
+    { text = _G.DUNGEON_DIFFICULTY, isTitle = true, notCheckable = true },
+    { text = _G.DUNGEON_DIFFICULTY1, checked = function() return GetDungeonDifficulty() == 1 end, func = function() SetDungeonDifficulty(1);  end },
+    { text = _G.DUNGEON_DIFFICULTY2, checked = function() return GetDungeonDifficulty() == 2 end, func = function() SetDungeonDifficulty(2);  end },
+    { text = '', isTitle = true, notCheckable = true },
+    { text = _G.RAID_DIFFICULTY, isTitle = true, notCheckable = true},
+    { text = _G.RAID_DIFFICULTY1, checked = function() return GetRaidDifficulty() == 1 end, func = function() SetRaidDifficulty(1);  end },
+    { text = _G.RAID_DIFFICULTY2, checked = function() return GetRaidDifficulty() == 2 end, func = function() SetRaidDifficulty(2);  end },
+    { text = _G.RAID_DIFFICULTY3, checked = function() return GetRaidDifficulty() == 3 end, func = function() SetRaidDifficulty(3);  end },
+    { text = _G.RAID_DIFFICULTY4, checked = function() return GetRaidDifficulty() == 4 end, func = function() SetRaidDifficulty(4);  end },
+    { text = '', isTitle = true, notCheckable = true },
+    { text = _G.RESET_INSTANCES, notCheckable = true, func = function() ResetInstances();  end},
 }
 
-local IDTexture = {
-	LEGACY = { 3, 4, 5, 6, 9 },
-	RAID = { 14, 15, 16 },
+local DiffDungLabel, DiffRaidLabel = {
+    [1] = _G.DUNGEON_DIFFICULTY1,
+    [2] = _G.DUNGEON_DIFFICULTY2,
+}, {
+    [1] = _G.RAID_DIFFICULTY1,
+    [2] = _G.RAID_DIFFICULTY2,
+	[3] = _G.RAID_DIFFICULTY3,
+	[4] = _G.RAID_DIFFICULTY4,
 }
 
-local Garrison = {
-	[1152] = true,
-	[1330] = true,
-	[1153] = true,
-	[1154] = true,
-	[1158] = true,
-	[1331] = true,
-	[1159] = true,
-	[1160] = true,
-}
+local function GetDiffDungInfo(difficultyID)
+    local heroicD = (GetDungeonDifficulty() > 1) and heroicTex or ''
+    local label = format('%s %s', DiffDungLabel[difficultyID]:gsub('%s*%([^%)]*%)', ''), heroicD)
 
-local function GetDiffIDLabel(ID)
-	for Name, Info in pairs(DiffIDLabel) do
-		for _, Num in pairs(Info) do
-			if Num == ID then
-				return Name
-			end
-		end
-	end
-	return ID
+    return label
 end
 
-local function GetLabelTexture(ID)
-	for Name, Info in pairs(IDTexture) do
-		for _, Num in pairs(Info) do
-			if Num == ID then
-				return (Name == 'LEGACY' and LegacyTexture) or RaidTexture
-			end
-		end
-	end
-	return DungeonTexture
-end
+local function GetDiffRaidInfo(difficultyID)
+    local heroicR = (GetRaidDifficulty() > 2) and heroicTex or ''
+    local label = format('%s %s', DiffRaidLabel[difficultyID]:gsub('%s*%([^%)]*%)', ''), heroicR)
 
-local function OnClick(self)
-	E:SetEasyMenuAnchor(E.EasyMenu, self)
-	EasyMenu(RightClickMenu, E.EasyMenu, nil, nil, nil, 'MENU')
+    return label
 end
 
 local function OnEvent(self)
-	local name, instanceType, difficultyID, _, _, _, _, instanceID = GetInstanceInfo()
-	local keyStoneLevel = E.Retail and C_MythicPlus_IsMythicPlusActive() and C_ChallengeMode_GetActiveChallengeMapID() and C_ChallengeMode_IsChallengeModeActive() and C_ChallengeMode_GetActiveKeystoneInfo()
+    local name, instanceType = GetInstanceInfo()
 
-	if keyStoneLevel then
-		self.text:SetFormattedText('%s %s +%s', GetLabelTexture(difficultyID), name, keyStoneLevel)
-	elseif instanceType ~= 'none' and difficultyID and not Garrison[instanceID] then
-		self.text:SetFormattedText('%s %s %s', GetLabelTexture(difficultyID), name, GetDiffIDLabel(difficultyID))
-	else
-		DungeonDifficultyID, RaidDifficultyID, LegacyRaidDifficultyID = GetDungeonDifficultyID(), GetRaidDifficultyID(), E.Retail and GetLegacyRaidDifficultyID()
-		local displayString = E.Retail and '%s %s %s %s %s %s' or '%s %s %s %s'
-		self.text:SetFormattedText(displayString, DungeonTexture, GetDiffIDLabel(DungeonDifficultyID), RaidTexture, GetDiffIDLabel(RaidDifficultyID), E.Retail and LegacyTexture, E.Retail and GetDiffIDLabel(LegacyRaidDifficultyID))
-	end
+    if instanceType == 'none' then
+        self.text:SetFormattedText('%s %s %s %s', dungTex, GetDiffDungInfo(GetDungeonDifficulty()), raidTex, GetDiffRaidInfo(GetRaidDifficulty()))
+    else
+        self.text:SetFormattedText('%s: %s', name, (instanceType == 'raid') and GetDiffRaidInfo(GetRaidDifficulty()) or GetDiffDungInfo(GetDungeonDifficulty()))
+    end
+end
+
+local function OnClick(self)
+    E:SetEasyMenuAnchor(E.EasyMenu, self)
+	EasyMenu(RightClickMenu, E.EasyMenu, nil, nil, nil, 'MENU')
 end
 
 local function OnEnter()
-	if not (DungeonDifficultyID or RaidDifficultyID or LegacyRaidDifficultyID) then return end
+    DT.tooltip:ClearLines()
 
-	DT.tooltip:ClearLines()
-	DT.tooltip:SetText(L["Current Difficulties:"])
-	DT.tooltip:AddLine(' ')
+    DT.tooltip:AddLine(L['Current Difficulty'])
+    DT.tooltip:AddLine(' ')
+    DT.tooltip:AddDoubleLine(_G.DUNGEON_DIFFICULTY, GetDiffDungInfo(GetDungeonDifficulty()), 1, 1, 1)
+    DT.tooltip:AddDoubleLine(_G.RAID_DIFFICULTY, GetDiffRaidInfo(GetRaidDifficulty()), 1, 1, 1)
 
-	if DungeonDifficultyID then
-		DT.tooltip:AddLine(format('%s %s', DungeonTexture, GetDiffIDLabel(DungeonDifficultyID)), 1, 1, 1)
-	end
-	if RaidDifficultyID then
-		DT.tooltip:AddLine(format('%s %s', RaidTexture, GetDiffIDLabel(RaidDifficultyID)), 1, 1, 1)
-	end
-	if LegacyRaidDifficultyID then
-		DT.tooltip:AddLine(format('%s %s', LegacyTexture, GetDiffIDLabel(LegacyRaidDifficultyID)), 1, 1, 1)
-	end
-
-	DT.tooltip:Show()
+    DT.tooltip:Show()
 end
 
-DT:RegisterDatatext('Difficulty', nil, {'CHAT_MSG_SYSTEM', 'LOADING_SCREEN_DISABLED'}, OnEvent, nil, OnClick, OnEnter, nil, 'Difficulty')
+DT:RegisterDatatext('Difficulty', nil, {'CHAT_MSG_SYSTEM'}, OnEvent, nil, OnClick, OnEnter, nil, 'Difficulty')
