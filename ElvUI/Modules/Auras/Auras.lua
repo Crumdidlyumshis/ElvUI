47f1e42140ae653f9825f4835479e26f536d2478
SvenBledt <sven.bledt@gmail.com>
2025-10-10 18:44:39 +0200

    Refactor aura configuration to use localized database references for improved clarity and maintainability

--- ElvUI/Modules/Auras/Auras.lua
+++ ElvUI/Modules/Auras/Auras.lua
@@ -77,7 +77,8 @@ function A:UpdateTime(elapsed)
 		return
 	end
 
-	if self.statusBar:IsShown() and A.db.barColorGradient then
+	local db = A.db[self.auraType]
+	if self.statusBar:IsShown() and db.barColorGradient then
 		self.statusBar:SetStatusBarColor(E.oUF:ColorGradient(self.timeLeft, self.duration or 0, .8, 0, 0, .8, .8, 0, 0, .8, 0))
 	end
 
@@ -321,17 +322,17 @@ function A:ConfigureAuras(header, auraTable, weaponPosition)
 			button.count:SetText("")
 		end
 
-		if (A.db.barShow and buffInfo.duration > 0) or (A.db.barShow and A.db.barNoDuration and buffInfo.duration == 0) then
+		if (db.barShow and buffInfo.duration > 0) or (db.barShow and db.barNoDuration and buffInfo.duration == 0) then
 			button.statusBar:Show()
 
-			if not button.timeLeft or not A.db.barColorGradient then
-				button.statusBar:SetStatusBarColor(A.db.barColor.r, A.db.barColor.g, A.db.barColor.b)
+			if not button.timeLeft or not db.barColorGradient then
+				button.statusBar:SetStatusBarColor(db.barColor.r, db.barColor.g, db.barColor.b)
 			end
 		else
 			button.statusBar:Hide()
 		end
 
-		button.text:SetShown(A.db.showDuration)
+		button.text:SetShown(db.showDuration)
 
 		if buffInfo.filter == "HARMFUL" then
 			local color = DebuffTypeColor[buffInfo.dispelType or ""]
@@ -381,17 +382,17 @@ function A:ConfigureAuras(header, auraTable, weaponPosition)
 						A:ClearAuraTime(button)
 					end
 
-					if (A.db.barShow and duration > 0) or (A.db.barShow and A.db.barNoDuration and duration == 0) then
+					if (db.barShow and duration > 0) or (db.barShow and db.barNoDuration and duration == 0) then
 						button.statusBar:Show()
 
-						if not button.timeLeft or not A.db.barColorGradient then
-							button.statusBar:SetStatusBarColor(A.db.barColor.r, A.db.barColor.g, A.db.barColor.b)
+						if not button.timeLeft or not db.barColorGradient then
+							button.statusBar:SetStatusBarColor(db.barColor.r, db.barColor.g, db.barColor.b)
 						end
 					else
 						button.statusBar:Hide()
 					end
 
-					if A.db.showDuration then
+					if db.showDuration then
 						button.text:Show()
 					else
 						button.text:Hide()
@@ -416,7 +417,7 @@ function A:ConfigureAuras(header, auraTable, weaponPosition)
 		display = min(display, wrapAfter * maxWraps)
 	end
 
-	local pos, spacing, iconSize = A.db.barPosition, A.db.barSpacing, db.size - (E.Border * 2)
+	local pos, spacing, iconSize = db.barPosition, db.barSpacing, db.size - (E.Border * 2)
 	local isOnTop = pos == "TOP" and true or false
 	local isOnBottom = pos == "BOTTOM" and true or false
 	local isOnLeft = pos == "LEFT" and true or false
@@ -432,22 +433,23 @@ function A:ConfigureAuras(header, auraTable, weaponPosition)
 		button:SetSize(size, size)
 
 		if button.text then
-			local db = A.db[button.auraType]
+			local buttonDb = A.db[button.auraType]
 
 			button.text:ClearAllPoints()
-			button.text:Point("TOP", button, "BOTTOM", 1 + A.db.timeXOffset, 0 + A.db.timeYOffset)
-			button.text:FontTemplate(LSM:Fetch('font', db.timeFont), db.timeFontSize, db.timeFontOutline)
+			button.text:Point("TOP", button, "BOTTOM", 1 + buttonDb.timeXOffset, 0 + buttonDb.timeYOffset)
+			button.text:FontTemplate(LSM:Fetch('font', buttonDb.timeFont), buttonDb.timeFontSize, buttonDb.timeFontOutline)
 
 			button.count:ClearAllPoints()
-			button.count:Point("BOTTOMRIGHT", -1 + A.db.countXOffset, 0 + A.db.countYOffset)
-			button.count:FontTemplate(LSM:Fetch('font', db.countFont), db.countFontSize, db.countFontOutline)
+			button.count:Point("BOTTOMRIGHT", -1 + buttonDb.countXOffset, 0 + buttonDb.countYOffset)
+			button.count:FontTemplate(LSM:Fetch('font', buttonDb.countFont), buttonDb.countFontSize, buttonDb.countFontOutline)
 		end
 
-		button.statusBar:Width((isOnTop or isOnBottom) and iconSize or (A.db.barWidth + (E.PixelMode and 0 or 2)))
-		button.statusBar:Height((isOnLeft or isOnRight) and iconSize or (A.db.barHeight + (E.PixelMode and 0 or 2)))
+		local barSize = db.barSize + (E.PixelMode and 0 or 2)
+		button.statusBar:Width((isOnTop or isOnBottom) and iconSize or barSize)
+		button.statusBar:Height((isOnLeft or isOnRight) and iconSize or barSize)
 		button.statusBar:ClearAllPoints()
 		button.statusBar:Point(E.InversePoints[pos], button, pos, (isOnTop or isOnBottom) and 0 or ((isOnLeft and -((E.PixelMode and 1 or 3) + spacing)) or ((E.PixelMode and 1 or 3) + spacing)), (isOnLeft or isOnRight) and 0 or ((isOnTop and ((E.PixelMode and 1 or 3) + spacing) or -((E.PixelMode and 1 or 3) + spacing))))
-		button.statusBar:SetStatusBarTexture(LSM:Fetch("statusbar", A.db.barTexture))
+		button.statusBar:SetStatusBarTexture(LSM:Fetch("statusbar", db.barTexture))
 		if isOnLeft or isOnRight then
 			button.statusBar:SetOrientation("VERTICAL")
 		else
